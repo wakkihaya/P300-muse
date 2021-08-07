8f4a672fce9917d25c89f143fe47abddd209f844
wakkihaya <wakkihaya@gmail.com>
2021-08-07 18:53:01 +0900

    wip: show epoch plog

--- P300.py
+++ P300.py
@@ -1,7 +1,7 @@
 import sys
 from collections import OrderedDict
 
-from mne import create_info, concatenate_raws
+from mne import create_info, concatenate_raws, Epochs, find_events
 from mne.io import RawArray
 from mne.channels import read_custom_montage
 
@@ -15,7 +15,27 @@ import utils
 
 if __name__ == "__main__":
     subject = 1
+    # Read raw data from muse device
     raw = utils.load_data('p300', sfreq=256.,
                           subject_nb=subject,
                           ch_ind=[0, 1, 2, 3])
-    print(raw)
+
+    # raw.plot_psd(tmax=np.inf)
+
+    raw.filter(1, 30, method='iir')    # Filter by 30 Hz
+
+    events = find_events(raw)
+    event_id = {'Non-Target': 1, 'Target': 2}
+
+    epochs = Epochs(raw, events=events, event_id=event_id, tmin=-0.1, tmax=0.8, baseline=None,
+                    reject={'eeg': 100e-6}, preload=True, verbose=False, picks=[0, 1, 2, 3])
+
+    # Epoch average
+    conditions = OrderedDict()
+    conditions['Non-target'] = [1]
+    conditions['Target'] = [2]
+
+    # //TODO: not showing plot
+    fig, ax = utils.plot_conditions(epochs, conditions=conditions,
+                                    ci=97.5, n_boot=1000, title='',
+                                    diff_waveform=(1, 2))
--- utils.py
+++ utils.py
@@ -10,6 +10,9 @@ import numpy as np
 import seaborn as sns
 from matplotlib import pyplot as plt
 
+sns.set_context('talk')
+sns.set_style('white')
+
 
 def load_data(data_dir, subject_nb=1, sfreq=256.,
               ch_ind=[0, 1, 2, 3], stim_ind=5, replace_ch_names=None):
@@ -75,7 +78,6 @@ def load_muse_csv_as_raw(filepath, sfreq=256., ch_ind=[0, 1, 2, 3],
 
     # type of each channels
     ch_types = ['eeg'] * n_channel + ['stim']
-    montage = make_standard_montage('standard_1005')
 
     # get data and exclude Aux channel
     data = data.values[:, ch_ind + [stim_ind]].T
@@ -92,3 +94,76 @@ def load_muse_csv_as_raw(filepath, sfreq=256., ch_ind=[0, 1, 2, 3],
     raws = concatenate_raws(raw)
 
     return raws
+
+
+def plot_conditions(epochs, conditions=OrderedDict(), ci=97.5, n_boot=1000,
+                    title='', palette=None, ylim=(-6, 6),
+                    diff_waveform=(1, 2)):
+    """Plot ERP conditions.
+    Args:
+        epochs (mne.epochs): EEG epochs
+    Keyword Args:
+        conditions (OrderedDict): dictionary that contains the names of the
+            conditions to plot as keys, and the list of corresponding marker
+            numbers as value. E.g.,
+                conditions = {'Non-target': [0, 1],
+                               'Target': [2, 3, 4]}
+        ci (float): confidence interval in range [0, 100]
+        n_boot (int): number of bootstrap samples
+        title (str): title of the figure
+        palette (list): color palette to use for conditions
+        ylim (tuple): (ymin, ymax)
+        diff_waveform (tuple or None): tuple of ints indicating which
+            conditions to subtract for producing the difference waveform.
+            If None, do not plot a difference waveform
+    Returns:
+        (matplotlib.figure.Figure): figure object
+        (list of matplotlib.axes._subplots.AxesSubplot): list of axes
+    """
+    if isinstance(conditions, dict):
+        conditions = OrderedDict(conditions)
+
+    if palette is None:
+        palette = sns.color_palette("hls", len(conditions) + 1)
+
+    X = epochs.get_data() * 1e6
+    times = epochs.times
+    y = pd.Series(epochs.events[:, -1])
+
+    fig, axes = plt.subplots(2, 2, figsize=[12, 6],
+                             sharex=True, sharey=True)
+    axes = [axes[1, 0], axes[0, 0], axes[0, 1], axes[1, 1]]
+
+    for ch in range(4):
+        for cond, color in zip(conditions.values(), palette):
+            sns.tsplot(X[y.isin(cond), ch], time=times, color=color,
+                       n_boot=n_boot, ci=ci, ax=axes[ch])
+
+        if diff_waveform:
+            diff = (np.nanmean(X[y == diff_waveform[1], ch], axis=0) -
+                    np.nanmean(X[y == diff_waveform[0], ch], axis=0))
+            axes[ch].plot(times, diff, color='k', lw=1)
+
+        axes[ch].set_title(epochs.ch_names[ch])
+        axes[ch].set_ylim(ylim)
+        axes[ch].axvline(x=0, ymin=ylim[0], ymax=ylim[1], color='k',
+                         lw=1, label='_nolegend_')
+
+    axes[0].set_xlabel('Time (s)')
+    axes[0].set_ylabel('Amplitude (uV)')
+    axes[-1].set_xlabel('Time (s)')
+    axes[1].set_ylabel('Amplitude (uV)')
+
+    if diff_waveform:
+        legend = (['{} - {}'.format(diff_waveform[1], diff_waveform[0])] +
+                  list(conditions.keys()))
+    else:
+        legend = conditions.keys()
+    axes[-1].legend(legend)
+    sns.despine()
+    plt.tight_layout()
+
+    if title:
+        fig.suptitle(title, fontsize=20)
+
+    return fig, axes
